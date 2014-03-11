9a00eb562e0a2a3405fd7833c157b6ac0a0739cc
Jess Chadwick <jesschadwick@gmail.com>
2014-03-11 00:31:41 -0400

    Stripping the website project to the essentials

--- Website/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace AutomatedTesting.Website.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Website/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace AutomatedTesting.Website.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
